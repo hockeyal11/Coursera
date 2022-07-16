ece4b170b016be503404ba03374c48026195f5b7
hockeyal11 <97704736+hockeyal11@users.noreply.github.com>
2022-07-16 10:15:57 -0400

    Update Module_4_RPi.py

--- Module_4_RPi.py
+++ Module_4_RPi.py
@@ -1,38 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 
-class Control:
-    def __init__(self):
-        self.LedPin = 13
-        self.Button = 12
-        
-    def setup(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.LedPin, GPIO.OUT)
-        GPIO.setup(self.Button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.output(self.LedPin, GPIO.HIGH)
-        
-    def destroy(self):
-        GPIO.output(self.LedPin, GPIO.HIGH)
+GPIO.setmode(GPIO.BOARD)
+
+ledPin = 11
+buttonPin = 15
+
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+while True:
+    try:
+        if GPIO.input(buttonPin) == False:
+            GPIO.output(ledPin, GPIO.LOW)
+            time.sleep(1)
+            GPIO.output(ledPin, GPIO.HIGH)
+            time.sleep(1)
+            print('Button not pressed')
+        elif GPIO.input(buttonPin) == True:
+            GPIO.output(ledPin, GPIO.HIGH)
+            print('Button pressed')
+    except KeyboardInterrupt:
         GPIO.cleanup()
-        
-    def loop(self):
-        GPIO.output(self.LedPin, True)
-        try:
-            while True:
-                button_state = GPIO.input(self.Button)
-                if button_state == False:
-                    GPIO.output(self.LedPin, False)
-                    print("Button Pressed")
-                    time.sleep(0.3)
-                else:
-                    GPIO.output(self.LedPin, True)
-                    print("Button not Pressed")
-                    time.sleep(0.3)
-        except KeyboardInterrupt:
-            self.destroy()
-        
-if __name__ == '__main__':
-    obj = Control()
-    obj.setup()
-    obj.loop()
+        print('\n STOPPED')
+        exit()
